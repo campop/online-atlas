9e4e0773373151f24145f796962d2ff1d74759ee
mvl22 <martin@lucas-smith.co.uk>
2017-04-21 15:45:51 +0100

    Move homepage into framework

--- fertilityAtlas.php
+++ fertilityAtlas.php
@@ -43,8 +43,45 @@ class fertilityAtlas extends frontControllerApplication
 	public function home ()
 	{
 		# Start the HTML
-		$html = 'Home page';
-		
+		$html = '
+			
+			<p><em>Project under development.</em></p>
+			
+			<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+			<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
+			<link rel="stylesheet" href="http://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css" type="text/css">
+			
+			<link rel="stylesheet" href="https://unpkg.com/leaflet@1.0.3/dist/leaflet.css" />
+			<script src="https://unpkg.com/leaflet@1.0.3/dist/leaflet.js"></script>
+			
+			<script type="text/javascript" src="js/lib/geocoder/geocoder.js"></script>
+			
+			<script type="text/javascript" src="js/lib/leaflet-fullHash/leaflet-fullHash.js"></script>
+			
+			<script type="text/javascript" src="js/lib/leaflet-ajax/dist/leaflet.ajax.min.js"></script>
+			<script type="text/javascript" src="js/lib/LeafletSlider/leaflet.SliderControl.min.js"></script>
+			
+			<link rel="stylesheet" href="css/fertilityatlas.css" />
+			<script type="text/javascript" src=".config.js"></script>
+			<script type="text/javascript" src="js/fertilityatlas.js"></script>
+			<script type="text/javascript">
+				$(function() {
+					fertilityatlas.initialise (config);
+				});
+			</script>
+			
+			<div id="fertilityatlas">
+				
+				
+				<div id="geocoder">
+					<input type="text" name="location" autocomplete="off" placeholder="Search locations and move map" tabindex="1" />
+				</div>
+				
+				<div id="map"></div>
+				
+				
+			</div>
+		';
 		
 		# Show the HTML
 		echo $html;
