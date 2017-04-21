8fbb8ae2ef1fb98b32e8252a33acf6734de560a7
mvl22 <martin@lucas-smith.co.uk>
2017-04-21 17:23:54 +0100

    Add shapefile conversion script

---
+++ data/convert.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+releasedate=170421
+
+years=(
+	1851
+	1861
+	1881
+	1891
+	1901
+	1911
+)
+
+
+# Get current directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Convert each shapefile
+for year in ${years[@]} ; do
+	cd "${DIR}/_originals/${releasedate}/TFR_${year}/"
+	ogr2ogr -f GeoJSON -t_srs EPSG:4326 "${DIR}/${year}.geojson" *.shp
+	cd "${DIR}/"
+done
