92affd308bd413904e52f2fd539841566b28fb15
mvl22 <martin@lucas-smith.co.uk>
2017-04-23 14:49:29 +0100

    Remove any existing GeoJSON file(s) before regenerating

--- data/convert.sh
+++ data/convert.sh
@@ -17,6 +17,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Convert each shapefile
 for year in ${years[@]} ; do
+	rm -f "${DIR}/${year}.geojson"
 	cd "${DIR}/_originals/${releasedate}/TFR_${year}/"
 	ogr2ogr -f GeoJSON -t_srs EPSG:4326 "${DIR}/${year}.geojson" *.shp
 	cd "${DIR}/"
