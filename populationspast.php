6a8be75925d7dd663fed2b048107f12715ab3fbc
mvl22 <martin@lucas-smith.co.uk>
2017-08-21 18:15:08 +0100

    Update jQuery library

--- populationspast.php
+++ populationspast.php
@@ -146,7 +146,7 @@ class populationspast extends frontControllerApplication
 		# Start the HTML
 		$html = '
 			
-			<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+			<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 			<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
 			<link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css" type="text/css">
 			
