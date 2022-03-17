ea1ee1f5874b000f47678777b2e729b4c38c05fd
mvl22 <martin@lucas-smith.co.uk>
2022-03-17 13:42:21 +0000

    Increase northern max bounds to avoid preventing access to Shetland Islands

--- js/onlineatlas.js
+++ js/onlineatlas.js
@@ -29,7 +29,7 @@ var onlineatlas = (function ($) {
 		minZoom: 5,		// Zoomed out
 		
 		// Max bounds
-		maxBounds: [[47, -14], [60, 12]],	// South, West ; North, East
+		maxBounds: [[47, -14], [62, 12]],	// South, West ; North, East
 		
 		// Tileservers; historical map sources are listed at: http://wiki.openstreetmap.org/wiki/National_Library_of_Scotland
 		defaultTileLayer: 'bartholomew',
