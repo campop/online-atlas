d6505bddddfe39c06411fb2f05273c8087e16272
mvl22 <martin@lucas-smith.co.uk>
2026-03-12 17:29:24 +0000

    Fix valueUnknown = NULL support

--- onlineAtlas.php
+++ onlineAtlas.php
@@ -40,7 +40,7 @@ class onlineAtlas extends frontControllerApplication
 			'defaultVariations' => array (),
 			'datasetsAttribution' => 'Campop',
 			'intervalsMode' => false,
-			'valueUnknown' => NULL,	// For all decimal fields, special value which represents unknown data
+			'valueUnknown' => NULL,	// For all decimal fields, special value which represents unknown data; if the real value is NULL, set it to string 'NULL' and this will be treated as real NULL
 			'valueUnknownString' => 'Unknown',
 			'colourUnknown' => '#c8c8c8',
 			'variations' => array (),	// As variation-label => array (field => label), variation-label...
@@ -133,6 +133,11 @@ class onlineAtlas extends frontControllerApplication
 			}
 		}
 		
+		# If the value is 'NULL', treat as real NULL
+		if ($this->settings['valueUnknown'] == 'NULL') {
+			$this->settings['valueUnknown'] = NULL;
+		}
+		
 	}
 	
 	
@@ -205,6 +210,21 @@ class onlineAtlas extends frontControllerApplication
 	# Welcome screen
 	public function home ($additionalCss = false)
 	{
+		# Preformat valueUnknown
+		switch (true) {
+			case is_bool ($this->settings['valueUnknown']):
+			case is_numeric ($this->settings['valueUnknown']):
+				$valueUnknownJS = $this->settings['valueUnknown'];
+				break;
+				
+			case is_null ($this->settings['valueUnknown']):
+				$valueUnknownJS = 'null';
+				break;
+				
+			default:
+				$valueUnknownJS = "'{$this->settings['valueUnknown']}'";
+		}
+		
 		# Start the HTML
 		$html = '
 			
@@ -256,7 +276,7 @@ class onlineAtlas extends frontControllerApplication
 					fields: ' . json_encode ($this->settings['fields'], JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES) . ',
 					colourStops: ' . json_encode ($this->settings['colourStops']) . ',
 					intervalsMode: ' . ($this->settings['intervalsMode'] ? 'true' : 'false') . ',
-					valueUnknown: ' . ((is_bool ($this->settings['valueUnknown']) || is_null ($this->settings['valueUnknown']) || is_numeric ($this->settings['valueUnknown'])) ? $this->settings['valueUnknown'] : "'{$this->settings['valueUnknown']}'") . ',
+					valueUnknown: ' . $valueUnknownJS . ',
 					valueUnknownString: ' . ($this->settings['valueUnknownString'] ? "'{$this->settings['valueUnknownString']}'" : 'false') . ',
 					colourUnknown: ' . ($this->settings['colourUnknown'] ? "'{$this->settings['colourUnknown']}'" : 'false') . ',
 					export: ' . ($this->settings['downloadFilenameBase'] ? 'true' : 'false') . ',
